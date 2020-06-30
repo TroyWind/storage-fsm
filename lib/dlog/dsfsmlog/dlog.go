fb7179c38f7ee777436d80d51c3ee40c1bdcdaa8
刘林欣 <llx@ipfsmain.cn>
2020-06-30 18:06:25 +0800

    add log

---
+++ lib/dlog/dsfsmlog/dlog.go
@@ -0,0 +1,12 @@
+package dsfsmlog
+
+import (
+	"github.com/filecoin-project/storage-fsm/lib/util"
+	"go.uber.org/zap"
+)
+
+var L *zap.Logger
+
+func init() {
+	L = util.GetXDebugLog("data-trans")
+}
